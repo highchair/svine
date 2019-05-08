9ff1391946dcd62a42658566e4b3f2ec67bceb18
kangabell <k@kangabell.co>
2019-05-08 13:59:33 -0400

    add logo, menu, and search form to site footer

--- footer.php
+++ footer.php
@@ -14,6 +14,25 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
+		<div class="site-branding">
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php the_custom_logo(); ?></a></p>
+		</div><!-- .site-branding -->
+
+		<nav class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'svine' ); ?></button>
+			<?php
+			wp_nav_menu( array(
+				'theme_location' => 'menu-1',
+				'menu_id'        => 'primary-menu',
+				'depth'			 => 1
+			) );
+			?>
+		</nav><!-- .main-navigation -->
+
+		<?php
+		get_search_form();
+		?>
+
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'svine' ) ); ?>">
 				<?php
