3a09ac9bd23ee39f4a30be731ca78d5fbc7a1d4a
kangabell <k@kangabell.co>
2019-05-08 15:47:22 -0400

    refine prev/next post nav on single post

--- single.php
+++ single.php
@@ -19,8 +19,33 @@ get_header();
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation();
+		?>
 
+		<nav class="navigation post-navigation" role="navigation">
+			<h2 class="screen-reader-text"><?php esc_html_e( 'Post navigation', 'svine' ); ?></h2>
+			<?php
+			if ( get_previous_post() ) :
+			?>
+				<div class="nav-previous">
+					<h3><?php esc_html_e( 'Previous', 'svine' ); ?></h3>
+					<?php previous_post_link( '%link' ); ?>
+				</div>
+			<?php
+			endif;
+			?>
+			<?php
+			if ( get_next_post() ) :
+			?>
+				<div class="nav-next">
+					<h3><?php esc_html_e( 'Next', 'svine' ); ?></h3>
+					<?php next_post_link( '%link' ); ?>
+				</div>
+			<?php
+			endif;
+			?>
+		</nav>
+
+		<?php
 		endwhile; // End of the loop.
 		?>
 
