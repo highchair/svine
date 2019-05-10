07b1d0b0ac8ac379014bbb8cb40e67cff6c9bcdb
kangabell <k@kangabell.co>
2019-05-10 13:59:42 -0400

    mobile site header layout

--- header.php
+++ header.php
@@ -40,7 +40,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'svine' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'svine' ); ?></button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
