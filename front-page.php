f2dd929b5119811a3d5d04c21cd41f7d56f484b7
kangabell <k@kangabell.co>
2019-07-30 16:11:56 -0400

    add Callout widget area to homepage

--- front-page.php
+++ front-page.php
@@ -14,10 +14,14 @@ get_header();
 		<main id="main" class="site-main">
 
 			<?php
+
 			while ( have_posts() ) :
 				the_post();
 				the_content();
 			endwhile;
+
+			dynamic_sidebar( 'callout' );
+
 			?>
 
 			<div>
