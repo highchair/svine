879e7010bf3024414ba1180d2eb95d7feb19d3e3
kangabell <k@kangabell.co>
2019-10-16 15:05:08 -0400

    mobile nav toggle button

--- header.php
+++ header.php
@@ -40,7 +40,9 @@
 			endif;
 			?>
 
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'svine' ); ?></button>
+			<button class="menu-toggle icon-button" aria-controls="primary-menu" aria-expanded="false">
+				<span class="screen-reader-text"><?php esc_html_e( 'Menu', 'svine' ); ?></span>
+			</button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
