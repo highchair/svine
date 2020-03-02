6bb2b502f4215552ad2a24c7e133515367157598
kangabell <k@kangabell.co>
2020-03-02 18:07:58 -0500

    remove navigation from footer; add social links placeholder

--- footer.php
+++ footer.php
@@ -15,39 +15,14 @@
 
 	<footer id="colophon" class="site-footer"><div class="wrap">
 
-		<nav class="footer-navigation main-navigation">
+		<div class="site-branding">
+			<a href="<?php echo home_url(); ?>"><?php include get_stylesheet_directory() . '/img/logo-badge.svg'; ?></a>
+			<div class="social-links">&nbsp;</div>
+		</div><!-- .site-branding -->
 
-			<div class="site-branding">
-				<a href="<?php echo home_url(); ?>"><?php include get_stylesheet_directory() . '/img/logo-badge.svg'; ?></a>
-			</div><!-- .site-branding -->
-
-			<div id="mobile-nav">
-				<?php
-				wp_nav_menu( array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'mobile-menu',
-					'depth'		 	 => 0
-				) );
-				get_search_form();
-				?>
-			</div>
-
-			<div class="desktop-nav">
-				<?php
-				wp_nav_menu( array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-					'container_class'	 => 'main-menu',
-					'depth'			 => 1
-				) );
-
-				get_search_form();
-				?>
-			</div>
-
-		</nav><!-- .footer-navigation -->
-
-		<?php dynamic_sidebar( 'footer' ); ?>
+		<div class="footer-widgets">
+			<?php dynamic_sidebar( 'footer' ); ?>
+		</div>
 
 		<div class="site-info">
 			<?php
